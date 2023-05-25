6c5e5482c50290900ffd14cbcd8005e870fba33d
Jingwen <jpan82@wisc.edu>
2023-05-25 14:23:43 -0500

    Add Geocoder

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-var map = L.map('map');
+let map = L.map('map');
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
 L.Routing.control({
+    // the begin coordinate
     waypoints: [
         L.latLng(43.074722, -89.406389),
         L.latLng(43.076424, -89.406722)
     ],
-    routeWhileDragging: true
+    routeWhileDragging: true, //allows users to drag a waypoint
+    geocoder: L.Control.Geocoder.nominatim(), // use the geocode
+    //router: L.Routing.graphHopper('b998e59b-7730-4892-a04c-db63ec9227a9'),
+    
 }).addTo(map);
